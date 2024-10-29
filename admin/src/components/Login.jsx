860188cb5c8353a20cf0d05fa62a536620bedc87
tarkeshwar raj <tarkeshwarraj@gmail.com>
2024-10-29 09:52:08 +0530

    admin

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -1,8 +1,8 @@
 import React,{ useState} from 'react'
 import axios from 'axios'
-import { backendUrl } from '../App.jsx'
+import { backendUrl } from '../App'
 
-const Login = () => {
+const Login = ({setToken}) => {
 
         const [email, setEmail] = useState('')
         const [password,setPassword] = useState('')
@@ -11,9 +11,14 @@ const Login = () => {
             try{
                 e.preventDefault();
                 const response = await axios.post(backendUrl + '/api/user/admin', {email,password})
-                console.log(response);
+                if (response.data.success){
+                    setToken(response.data.token)
+                }else{
+                    toast.error(response.data.message)
+                }
             }catch(error){
-                
+                console.log(error);
+                toast.error(error.message)
             }
         }
 
@@ -21,7 +26,7 @@ const Login = () => {
     <div className='min-h-screen flex items-center justify-center w-full'>
         <div className='bg-white shadow-md rounded-lg px-8 py-6 max-w-md'>
             <h1 className='text-2xl font-bold mb-4'>Admin Pannel</h1>
-            <form action="">
+            <form onSubmit={onSubmitHandler}>
                 <div className='mb-3 min-w-72'>
                     <p className='text-sm font medium text-gray-700 mb-2'>Email Address</p>
                     <input onChange={(e)=> setEmail(e.target.value)} value={email} className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none' type="email" placeholder='your@email.com' />
